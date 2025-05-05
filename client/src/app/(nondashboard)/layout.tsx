4ab0e010a45e37e26f500c267293b2793bd1c444
Anqi Guo <guo.anqi@icloud.com>
2025-05-05 22:50:12 +0800

    completed the hero section layout

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="h-full w-full">
       <Navbar />
       <main
         className={`h-full flex w-full flex-col`}
